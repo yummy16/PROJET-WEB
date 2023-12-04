7770ebd4ace340f34733be43108382acf97e79e2
yummy16 <99326041+yummy16@users.noreply.github.com>
2023-12-04 22:51:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+(function showResults(){
+     
+  //recupérer les éléments
+    const quizForm = document.getElementById('quiz-form');
+
+    quizForm.addEventListener('submit', function(event) {
+
+      const QuestContainers = document.querySelectorAll('.question-item');
+      let numCorrect = 0;
+
+      QuestContainers.forEach((QuestContainer, questionNumber) => {
+
+        const selector = `input[name=answer-${questionNumber + 1}]:checked`;
+        const userAnswer = (QuestContainer.querySelector(selector) || {}).value;
+        const correctAnswer = "true";
+
+        //conditions
+
+        if(userAnswer === correctAnswer){
+          numCorrect++;
+          QuestContainer.style.color = 'green';
+        }
+        else {
+          QuestContainer.style.color = 'red';
+        }
+
+        if (numCorrect === 3){
+          document.getElementById("alert").style.display = 'block';
+        }
+      });
+
+      event.preventDefault();
+      showResults();
+    });
+  })();
